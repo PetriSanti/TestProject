b30a88890dffdcd61005710c8e90357909e53615
PetriSanti <41927348+PetriSanti@users.noreply.github.com>
2018-09-02 00:44:13 +0200

    Add "read me" header file

---
+++ MKE06Z128xxx4_Project_Test/source/Readme.h
@@ -0,0 +1,13 @@
+/*
+ * Readme.h
+ *
+ *  Created on: 2 Sep 2018
+ *      Author: pedro
+ */
+
+#ifndef README_H_
+#define README_H_
+
+/* Describe instructions and main tasks to implement */
+
+#endif /* README_H_ */
