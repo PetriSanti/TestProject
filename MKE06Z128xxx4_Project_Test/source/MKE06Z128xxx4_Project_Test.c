eeb052e15236bebeb7f77b2c79e16ba9df22be04
almor <almor@DESKTOP-V7SNIQ4.lan>
2018-09-02 00:07:20 +0100

    Removed legal copyright clauses.

--- MKE06Z128xxx4_Project_Test/source/MKE06Z128xxx4_Project_Test.c
+++ MKE06Z128xxx4_Project_Test/source/MKE06Z128xxx4_Project_Test.c
@@ -5,13 +5,23 @@
  * Redistribution and use in source and binary forms, with or without modification,
  * are permitted provided that the following conditions are met:
  *
- * o Redistributions of source code must retain the above copyright notice, this list
- *   of conditions and the following disclaimer.
  *
  * o Redistributions in binary form must reproduce the above copyright notice, this
  *   list of conditions and the following disclaimer in the documentation and/or
- *   other materials provided with the distribution.
+/*
+ * Main tasks:
+ * 1-Primary functions
+ * - communication between MKE06z and bq76pl
+ * 		-pin: 1,2,3,4,38,39,40,41
+ * - read can address
+ * 		-pin: 11,12,14,15
+ * - communications between slaves via can bus
+ * 		-pin: 36,36
+ * - configure LED status
+ * 		-pin: 21
  *
+ * 2-Secondary functions
+  / *
  * o Neither the name of NXP Semiconductor, Inc. nor the names of its
  *   contributors may be used to endorse or promote products derived from this
  *   software without specific prior written permission.
