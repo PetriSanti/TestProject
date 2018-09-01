65895d30bcd70abc2b02c038a09e2eb7a14cfa43
almor <almor@DESKTOP-V7SNIQ4.lan>
2018-09-02 00:01:57 +0100

    main tasks added to Readme.h

--- MKE06Z128xxx4_Project_Test/source/Readme.h
+++ MKE06Z128xxx4_Project_Test/source/Readme.h
@@ -8,6 +8,20 @@
 #ifndef README_H_
 #define README_H_
 
-/* Describe instructions and main tasks to implement */
+/* Describe instructions and main tasks to implement */~
+/*
+ * Main tasks:
+ * 1-Primary functions
+ * - communication between MKE06z and bq76pl
+ * 		-pin: 1,2,3,4,38,39,40,41
+ * - read can address
+ * 		-pin: 11,12,14,15
+ * - communications between slaves via can bus
+ * 		-pin: 36,36
+ * - configure LED status
+ * 		-pin: 21
+ *
+ * 2-Secondary functions
+  /
 
 #endif /* README_H_ */
