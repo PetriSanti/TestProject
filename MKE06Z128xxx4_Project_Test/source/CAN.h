f5a175453801fec3e8fb9ea2fd6360bcb2c630f9
PetriSanti <41927348+PetriSanti@users.noreply.github.com>
2018-09-02 20:15:32 +0200

    Development of CheckCANAddr() function

--- MKE06Z128xxx4_Project_Test/source/CAN.h
+++ MKE06Z128xxx4_Project_Test/source/CAN.h
@@ -9,18 +9,26 @@
 #define CAN_H_
 
 #include "MKE06Z4.h"
+#include "GPIO.h"
+
+/* All CAN-bus related definitions and functions come here */
 
 /* Defines start here */
 
 /* Functions start here */
-void CheckCanAddr(){
+void CheckCANAddr(){
+	/* Variable CAN address detected from 4-bit resistor code on PCB
+	 * Each resistor is pulled-up to 5V, if no resistor pin reads low
+	 */
 
+	uint16_t canaddr = mGetAddrBit0();
+	canaddr |= ((mGetAddrBit1() << 1) | (mGetAddrBit2() << 2) | (mGetAddrBit3() << 3));
 	// TODO Attribute Slave or Master mode
 }
-void GetCanMsg(){
+void GetCANMsg(){
 
 }
-void SendCanMsg(){
+void SendCANMsg(){
 
 }
 
