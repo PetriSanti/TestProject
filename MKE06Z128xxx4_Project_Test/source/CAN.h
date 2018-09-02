eb6716231a1d19de4b444a333d47dcdf299b9f01
PetriSanti <41927348+PetriSanti@users.noreply.github.com>
2018-09-02 13:16:47 +0200

    New header file, all CAN-Bus related functions must be inserted here

---
+++ MKE06Z128xxx4_Project_Test/source/CAN.h
@@ -0,0 +1,27 @@
+/*
+ * CAN.h
+ *
+ *  Created on: 2 Sep 2018
+ *      Author: pedro
+ */
+
+#ifndef CAN_H_
+#define CAN_H_
+
+#include "MKE06Z4.h"
+
+/* Defines start here */
+
+/* Functions start here */
+void CheckCanAddr(){
+
+	// TODO Attribute Slave or Master mode
+}
+void GetCanMsg(){
+
+}
+void SendCanMsg(){
+
+}
+
+#endif /* CAN_H_ */
