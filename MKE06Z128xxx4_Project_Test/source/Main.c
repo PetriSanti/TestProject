54efbf0a01c3742fb1a8591bcba9e0c65b344ed9
PetriSanti <41927348+PetriSanti@users.noreply.github.com>
2018-09-02 18:52:09 +0200

    Change of package configuration to current 44 LQFP

--- MKE06Z128xxx4_Project_Test/source/Main.c
+++ MKE06Z128xxx4_Project_Test/source/Main.c
@@ -1,47 +1,9 @@
-/*
- * Copyright (c) 2017, NXP Semiconductor, Inc.
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted provided that the following conditions are met:
- *
- *
- * o Redistributions in binary form must reproduce the above copyright notice, this
- *   list of conditions and the following disclaimer in the documentation and/or
-/*
- * Main tasks:
- * 1-Primary functions
- * - communication between MKE06z and bq76pl
- * 		-pin: 1,2,3,4,38,39,40,41
- * - read can address
- * 		-pin: 11,12,14,15
- * - communications between slaves via can bus
- * 		-pin: 36,36
- * - configure LED status
- * 		-pin: 21
- *
- * 2-Secondary functions
-  / *
- * o Neither the name of NXP Semiconductor, Inc. nor the names of its
- *   contributors may be used to endorse or promote products derived from this
- *   software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
- * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
- * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
  
 /**
  * @file    MKE06Z128xxx4_Project_Test.c
  * @brief   Application entry point.
  */
+
 #include <stdio.h>
 #include "board.h"
 #include "peripherals.h"
